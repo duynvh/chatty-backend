721dc0bd72bd83cad77088734db6817668a9553b
duynvh <nguyenvohoangduy@gmail.com>
2022-09-19 16:55:51 +0700

    Feat: Implement follow, unfollow, block, unblock API

---
+++ src/shared/services/db/block-user.service.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import mongoose from 'mongoose';
+import { PushOperator } from 'mongodb';
+import { UserModel } from '@user/models/user.schema';
+
+class BlockUserService {
+  public async blockUser(userId: string, followerId: string): Promise<void> {
+    UserModel.bulkWrite([
+      {
+        updateOne: {
+          // @ts-ignore
+          filter: { _id: userId, blocked: { $ne: new mongoose.Types.ObjectId(followerId) } },
+          update: {
+            $push: {
+              blocked: new mongoose.Types.ObjectId(followerId)
+            } as PushOperator<Document>
+          }
+        }
+      },
+      {
+        updateOne: {
+          // @ts-ignore
+          filter: { _id: followerId, blockedBy: { $ne: new mongoose.Types.ObjectId(userId) } },
+          update: {
+            $push: {
+              blockedBy: new mongoose.Types.ObjectId(userId)
+            } as PushOperator<Document>
+          }
+        }
+      },
+    ]);
+  }
+
+  public async unblockUser(userId: string, followerId: string): Promise<void> {
+    UserModel.bulkWrite([
+      {
+        updateOne: {
+          // @ts-ignore
+          filter: { _id: userId },
+          update: {
+            $pull: {
+              blocked: new mongoose.Types.ObjectId(followerId)
+            } as PushOperator<Document>
+          }
+        }
+      },
+      {
+        updateOne: {
+          // @ts-ignore
+          filter: { _id: followerId },
+          update: {
+            $pull: {
+              blockedBy: new mongoose.Types.ObjectId(userId)
+            } as PushOperator<Document>
+          }
+        }
+      },
+    ]);
+  }
+}
+
+export const blockUserService: BlockUserService = new BlockUserService();
